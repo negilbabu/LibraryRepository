07c4594980fb073e7b7fcf93be865bd3cacde7b5
negilbabu <negil.babu@innovaturelabs.com>
2023-01-25 19:04:54 +0530

    testing

--- BackEnd/Library/src/main/java/com/innovature/Library/entity/csvUpload.java
+++ BackEnd/Library/src/main/java/com/innovature/Library/entity/csvUpload.java
@@ -1,5 +1,4 @@
 package com.innovature.Library.entity;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,161 +6,116 @@ import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 
 @Entity
-@Table(name="books")
+@Table(name = "books")
 
+public class csvUpload {
+    public static enum Status {
+        INACTIVE((byte) 0),
+        ACTIVE((byte) 1);
 
-public class csvUpload { public static enum Status {
-    INACTIVE((byte) 0),
-    ACTIVE((byte) 1);
+        public final byte value;
 
-    public final byte value;
-
-    private Status(byte value) {
-        this.value = value;
+        private Status(byte value) {
+            this.value = value;
+        }
     }
-}
-// @Id
-// @GeneratedValue(strategy = GenerationType.IDENTITY)
-// @Column(name="book_id")
-// private Long booksId;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "books_id")
+    private Integer booksId;
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(name="books_id")
-private Integer booksId;
+    @Column(name = "books_name")
+    private String booksName;
 
-@Column(name="books_name")
-private String booksName;
+    @Column(name = "auther")
+    private String booksAuther;
 
-@Column(name="auther")
-private String booksAuther;
+    @Column(name = "books_copies")
+    private Integer booksCopies;
 
-@Column(name="books_copies")
-private Integer booksCopies;
+    @Column(name = "status")
+    private Integer status;
 
-@Column(name = "status")
-private Integer status;
+    @Column(name = "publication")
+    private String publication;
 
-@Column(name="publication")
-private String publication;
+    @Column(name = "category_id")
+    private Integer categoryId;
 
-@Column(name="category_id")
-private Integer categoryId;
+    @Column(name = "image")
+    private String image;
 
-@Column(name="image")
-private String image;
+    public csvUpload() {
 
-public csvUpload(){
+    }
 
-}
+    public csvUpload(String booksName, String publication, String booksAuther, Integer booksCopies, Integer categoryId,
+            Integer status) {
+        this.booksName = booksName;
+        this.publication = publication;
+        this.booksAuther = booksAuther;
+        this.booksCopies = booksCopies;
 
+        this.categoryId = categoryId;
+        this.status = status;
 
-public csvUpload(String booksName,String publication, String booksAuther, Integer booksCopies,Integer categoryId,Integer status) {
-this.booksName = booksName;
-this.publication = publication;
-this.booksAuther = booksAuther;
-this.booksCopies = booksCopies;
+    }
 
-this.categoryId = categoryId;
-this.status=status;
-// this.image = image;
+    @Override
+    public String toString() {
+        return "Csv [ booksName=" + booksName + ", booksAuthor=" + booksAuther + ", booksCopies=" + booksCopies
+                + ", categoryId=" + categoryId + ", status=" + status + "]";
+    }
 
+    public String getBooksName() {
+        return booksName;
+    }
 
-}
+    public void setBooksName(String booksName) {
+        this.booksName = booksName;
+    }
 
-@Override
-public String toString() {
-return "Csv [ booksName=" + booksName + ", booksAuthor=" + booksAuther + ", booksCopies=" + booksCopies
-        + ", categoryId=" + categoryId + ", status=" + status + "]";
-}
+    public String getBooksAuther() {
+        return booksAuther;
+    }
 
+    public void setBooksAuther(String booksAuther) {
+        this.booksAuther = booksAuther;
+    }
 
-// public Long getBooksId() {
-//     return booksId;
-// }
+    public Integer getBooksCopies() {
+        return booksCopies;
+    }
 
+    public void setBooksCopies(Integer booksCopies) {
+        this.booksCopies = booksCopies;
+    }
 
-// public void setBooksId(Long booksId) {
-//     this.booksId = booksId;
-// }
+    public Integer getStatus() {
+        return status;
+    }
 
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 
-public String getBooksName() {
-    return booksName;
-}
+    public String getPublication() {
+        return publication;
+    }
 
+    public void setPublication(String publication) {
+        this.publication = publication;
+    }
 
-public void setBooksName(String booksName) {
-    this.booksName = booksName;
-}
+    public Integer getCategoryId() {
+        return categoryId;
+    }
 
-
-public String getBooksAuther() {
-    return booksAuther;
-}
-
-
-public void setBooksAuther(String booksAuther) {
-    this.booksAuther = booksAuther;
-}
-
-
-public Integer getBooksCopies() {
-    return booksCopies;
-}
-
-
-public void setBooksCopies(Integer booksCopies) {
-    this.booksCopies = booksCopies;
-}
-
-
-public Integer getStatus() {
-    return status;
-}
-
-
-public void setStatus(Integer status) {
-    this.status = status;
-}
-
-
-public String getPublication() {
-    return publication;
-}
-
-
-public void setPublication(String publication) {
-    this.publication = publication;
-}
-
-
-public Integer getCategoryId() {
-    return categoryId;
-}
-
-
-public void setCategoryId(Integer categoryId) {
-    this.categoryId = categoryId;
-}
-
-
-// public String getImage() {
-//     return image;
-// }
-
-
-// public void setImage(String image) {
-//     this.image = image;
-// }
-
-
-
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
 
 }
--- BackEnd/Library/src/main/java/com/innovature/Library/repository/EmailRepository.java
+++ BackEnd/Library/src/main/java/com/innovature/Library/repository/EmailRepository.java
@@ -2,36 +2,18 @@ package com.innovature.Library.repository;
 
 import java.util.Collection;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.innovature.Library.entity.Email;
 
-
-public interface EmailRepository extends Repository<Email,Integer> {
+public interface EmailRepository extends Repository<Email, Integer> {
 
     void deleteAll();
 
     Email save(Email email);
 
-    Collection<Email>findAll();
+    Collection<Email> findAll();
 
     Email findByEmail(String email);
 
-    // @Query(value = "SELECT * FROM email WHERE email=?",nativeQuery = true)
-    // Email findByEmailId(String email);
-    
-    // @Modifying
-    // @Transactional
-    // @Query(value = "INSERT INTO email(email,otp) VALUES(email=?1,otp=?2) ON DUPLICATE KEY UPDATE otp =123123",nativeQuery = true)
-    // void findByEmailId(String email, Email otp2);
-
-
-
-
-//     INSERT INTO table (key,col1) VALUES (1,2)
-// INSERT INTO email(email,otp) VALUES("negilbabu2030@gmail.com",111111) ON DUPLICATE KEY UPDATE otp = 100000;
-//   ON DUPLICATE KEY UPDATE col1 = 2;
 }
